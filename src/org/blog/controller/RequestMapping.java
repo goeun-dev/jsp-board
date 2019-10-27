eeda4a76aa287bfd8b02e437cf59092cf86de66a
goeun-dev <goeunlee.me@gmail.com>
2019-10-27 19:53:34 +0900

    [추가] RquestMapping 어노테이션

---
+++ src/org/blog/controller/RequestMapping.java
@@ -0,0 +1,15 @@
+package org.blog.controller;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequestMapping {
+
+	public String value();	// 경로
+	public String type();	// 전송 방식
+
+}
