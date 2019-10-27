311c6619e6b66dc59ddee01a263228ddb176486d
goeun-dev <goeunlee.me@gmail.com>
2019-10-27 23:50:18 +0900

    [추가] PageMaker

--- src/org/blog/dto/PageMaker.java
+++ src/org/blog/dto/PageMaker.java
@@ -2,4 +2,29 @@ package org.blog.dto;
 
 public class PageMaker {
 
+	private int start;
+	private int end;
+	private boolean prev, next;
+	private int total;
+	private PagingDTO paging;
+
+	// 생성자
+	public PageMaker(int total, PagingDTO paging) {
+		super();
+		this.total = total;
+		this.paging = paging;
+
+		// 시작 페이지, 끝 페이지
+		int tempEnd = (int) (Math.ceil(paging.getPage() / 10.0)) * 10;
+		this.start = tempEnd - 9;
+
+		int realEnd = (total / paging.getAmount()) + 1;
+
+		this.end = tempEnd > realEnd ? realEnd : tempEnd;
+
+		// prev, next
+		this.prev = this.start != 1;
+		this.next = this.end * paging.getAmount() < total;
+
+	}
 }
